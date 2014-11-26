95f4e630e51f56666da465eb12a583266f666594
selimok <selimok@gmail.com>
2014-11-26 09:20:25 +0100

    Add missing files

---
+++ batch-example/src/main/java/demo/configuration/BatchAdminConfiguration.java
@@ -0,0 +1,11 @@
+package demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath:/org/springframework/batch/admin/web/resources/servlet-config.xml",
+        "classpath:/org/springframework/batch/admin/web/resources/webapp-config.xml" })
+public class BatchAdminConfiguration {
+
+}
